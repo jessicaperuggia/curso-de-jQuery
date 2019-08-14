b3db9d17a5cf2231ac33e238bde497cbdc72ad5b
jcperuggia <jessica.peruggia@hotmail.com>
2019-08-14 16:09:55 -0300

    colocando cronometro e arrumando o código

--- js/main.js
+++ js/main.js
@@ -5,10 +5,11 @@ $(function(){
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+    inicializaMarcadores();
     $("#botao-reiniciar").click(reiniciaJogo);
 });
 
-function atualizaTamanhoFrase(){
+function atualizaTamanhoFrase() {
     var frase = $(".frase").text();
     var numPalavras = frase.split(" ").length;
     var tamanhoFrase = $("#tamanho-frase");
@@ -27,32 +28,51 @@ function inicializaContadores(){
     });
 }
 
-function inicializaCronometro (){
+function inicializaMarcadores(){
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+
+        if(digitado == comparavel){
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
+} 
+
+function inicializaCronometro(){
     var tempoRestante = $("#tempo-digitacao").text();
     campo.one("focus", function(){
-        var cronometroID = setInterval(function (){
+        var cronometroID = setInterval(function(){
             tempoRestante--;
             $("#tempo-digitacao").text(tempoRestante);
-            if(tempoRestante <1){
-                campo.attr("disabled", true);
+            if(tempoRestante < 1){
                 clearInterval(cronometroID);
-                campo.toggleClass("campo-desativado");
+                finalizaJogo();
             }
-        },1000);
+        }, 1000);
     });
 }
 
-$("#botao-reiniciar").click(reiniciaJogo);
+function finalizaJogo(){
+    campo.attr("disabled", true);
+    campo.toggleClass("campo-desativado");
+    inserePlacar();
+}
 
 function reiniciaJogo(){
-    campo.attr("disbled", false);
+    campo.attr("disabled", false);
     campo.val("");
-    $("#contador-palavras").text("0");
-    $("#contador-caracteres").text("0");
+    $("#contador-palavras").text(0);
+    $("#contador-caracteres").text(0);
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
     campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
 }
-
-
     
